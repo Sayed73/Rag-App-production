785305de17abdc435ebe5fc27c1b0eaf2ce8584f
Sayed73 <sayedelzoghpy64@gmail.com>
2025-12-04 20:17:16 +0300

    add routes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
+
+from routes import base
 
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to RagApp version 1.0!"}
+app.include_router(base.base_router)
---
+++ routes/base.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, APIRouter
+import os
+
+
+base_router = APIRouter(
+    prefix="/api/v1",
+)
+
+@base_router.get("/")
+async def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+    return {"message": f"Welcome to {app_name} version {app_version}!"}
